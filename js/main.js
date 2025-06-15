6e76c218dd132b53b46447e8406d5c99fb4a66ae
ChristianGWolff45 <ChristiangGWolff@gmail.com>
2025-06-15 01:04:10 -0400

    resources fixed

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 
 const arrowButton = document.getElementsByClassName('arrow-button')[0];
+console.log(arrowButton)
 const resource = document.getElementById('resources');
 const resourceArrow = function() {
     console.log(arrowButton.innerHTML);
@@ -11,5 +12,35 @@ const resourceArrow = function() {
         resource.style.height = '50px';
     }
 }
-
 arrowButton.onclick = resourceArrow;
+document.querySelectorAll('.plus-button').forEach(button =>  {
+    button.addEventListener('click', () => {
+        const workerDiv = button.parentElement;
+        const span = workerDiv.querySelector('span');
+        let number = parseInt(span.textContent);
+        number++;
+        span.innerHTML = numFormat(number);
+    });
+});
+
+document.querySelectorAll('.minus-button').forEach(button =>  {
+    button.addEventListener('click', () => {
+        const workerDiv = button.parentElement;
+        const span = workerDiv.querySelector('span');
+        let number = parseInt(span.textContent);
+        console.log(number);
+        number--;
+        span.innerHTML = numFormat(number);
+    });
+});
+
+const numFormat = num => {
+    if(num < 0){
+        return '0' + 0;
+    }
+    else if(num < 10){
+        return '0' + num;
+    } else {
+        return num;
+    }
+}
